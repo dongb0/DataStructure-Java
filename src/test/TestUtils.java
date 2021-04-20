6826a5d1b56edc7ad66b40978c79c2620fec284e
dongb0 <708848999@qq.com>
2021-04-20 14:02:13 +0800

    add TestUtils randInt generator(using java.util.Random)

---
+++ src/test/TestUtils.java
@@ -0,0 +1,23 @@
+package test;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+public class TestUtils {
+
+    static Random random = new Random();
+
+    public static int[] getRandInts(int num){
+        return getRandInts(num, Integer.MAX_VALUE);
+    }
+
+    public static int[] getRandInts(int num, int bound){
+        int[] list = new int[num];
+        random.setSeed(new Date().getTime());
+        for(int i = 0; i < num; i++)
+            list[i] = random.nextInt(bound);
+        return list;
+    }
+
+}
